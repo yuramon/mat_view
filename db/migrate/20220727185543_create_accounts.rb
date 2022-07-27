7174a1db6183fe345e701295d6588bcf3f2fa0db
yuramon <yuramon2405@gmail.com>
2022-07-27 22:11:41 +0300

    generated Accounts model

---
+++ db/migrate/20220727185543_create_accounts.rb
@@ -0,0 +1,10 @@
+class CreateAccounts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accounts do |t|
+      t.float :balance, default: 0
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
